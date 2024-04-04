bb4c27c71e0587e3ab5ba80028cffb52b3df8945
BraianAdrian <braian.adrian.gagliardo@gmail.com>
2024-04-04 15:53:16 -0300

    test

---
+++ dontQualify.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>
+
+
+    </title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+
+<body class="h-screen flex flex-col justify-between">
+    <header class="bg-red-700 mx-auto text-center py-2 w-full">
+
+        <h1 class="text-xl text-white font-bold mx-4 sm:mx-0">
+            Breaking: Millions of Seniors On Medicare Are Upgrading Their Plans To Claim A $2,500 Spending Card!
+        </h1>
+
+    </header>
+
+    <main class="text-center h-96 flex justify-center items-center ">
+        <p class=" text-black-800  text-2xl w-2/4 p-10 bg-slate-400/50 h-60 flex justify-center items-center ">Sorry you don't Qualify!</p>
+    </main>
+
+
+    <footer class="footer mt-12 footer-end p-4 mt-12 mb-2 text-center w-full mx-auto text-xs">
+        <div class="grid grid-flow-col gap-3 md:w-1/3 mx-auto my-8">
+            <a class="link link-hover" href="https://<?php echo $currentDomain; ?>/terms"> Terms & Conditions </a>
+            <p class="text-blue-500">|</p>
+            <a class="link link-hover" href="https://<?php echo $currentDomain; ?>/privacy"> Privacy Policy </a>
+
+            <p class="text-blue-500">|</p>
+            <a class="link link-hover" href="https://<?php echo $currentDomain; ?>/privacy.php#caresidents"> CCPA </a>
+        </div>
+        <div class="mt-8">
+            <p>Copyright Golden Years Benefits © 2024 - All right reserved
+            </p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
